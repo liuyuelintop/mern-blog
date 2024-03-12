4e57fad6358242bfa50871ce279bf61fa065cbb1
liuyuelintop <liuyuelintop@gmail.com>
2024-03-12 22:47:02 +1100

    create signup api routes

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js';  
+
 dotenv.config();
 
 const app =express();
+
+// middleware
+app.use(express.json());
+
+
 mongoose.connect(process.env.MONGO)
 .then(()=>{
     console.log('Connected to MongoDB');
@@ -18,3 +26,6 @@ const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}!`);
 })
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
