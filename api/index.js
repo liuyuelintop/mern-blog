5140b8c14fab47d042187bdc8acc7a0719c5358d
liuyuelintop <liuyuelintop@gmail.com>
2024-03-18 21:29:31 +1100

    add update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js';  
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app =express();
 
 // middleware
 app.use(express.json());
+app.use(cookieParser());
+
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use((err, req, res, next) => {
