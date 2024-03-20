118aa0947b5b0d33435d776d34fedac1c54b18aa
liuyuelintop <liuyuelintop@gmail.com>
2024-03-20 12:27:41 +1100

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -2,12 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js';  
+import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
-const app =express();
+const app = express();
 
 // middleware
 app.use(express.json());
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
@@ -26,17 +28,17 @@ app.use((err, req, res, next) => {
 });
 
 mongoose.connect(process.env.MONGO)
-.then(()=>{
-    console.log('Connected to MongoDB');
-})
-.catch((err)=>{
-    console.log(err);
-});
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 
 //environment variables
 const port = process.env.PORT || 3000;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}!`);
 })
 
--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -1,4 +1,4 @@
-import {Button, FileInput, Select, TextInput} from 'flowbite-react';
+import { Button, FileInput, Select, TextInput } from 'flowbite-react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 export default function CreatePost() {
@@ -7,7 +7,7 @@ export default function CreatePost() {
       <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
       <form className="flex flex-col gap-4">
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
-          <TextInput type='text' placeholder='Title' required id = 'title' className='flex-1'/>
+          <TextInput type='text' placeholder='Title' required id='title' className='flex-1' />
           <Select>
             <option value="uncategorized">Select Category</option>
             <option value="javascript">Javascript</option>
@@ -19,7 +19,7 @@ export default function CreatePost() {
           <FileInput type='file' accept='image/.*' />
           <Button type='button' gradientDuoTone='purpleToBlue' size='sm' outline>Upload</Button>
         </div>
-        <ReactQuill theme='snow' placeholder='Write something...' className='h-72 mb-12' required/>
+        <ReactQuill theme='snow' placeholder='Write something...' className='h-72 mb-12' required />
         <Button type='submit' gradientDuoTone='purpleToPink'>Publish</Button>
       </form>
     </div>
