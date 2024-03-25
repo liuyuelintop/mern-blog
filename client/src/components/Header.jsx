b18807ec471897921c1037355456ad33a18e0ebb
liuyuelintop <liuyuelintop@gmail.com>
2024-03-25 20:33:46 +1100

    complete search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,68 +1,92 @@
 import { Button, Dropdown, Navbar, TextInput, Avatar } from 'flowbite-react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { FaMoon, FaSun } from 'react-icons/fa'
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import { signoutSuccess } from '../redux/user/userSlice';
+import { useState, useEffect } from 'react';
 export default function Header() {
     const path = useLocation().pathname;
+    const location = useLocation();
     const { currentUser } = useSelector((state) => state.user);
     const dispatch = useDispatch();
-    const {theme} = useSelector((state) => state.theme);
+    const { theme } = useSelector((state) => state.theme);
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
+
     const handleSignout = async () => {
         try {
             const res = await fetch('/api/user/signout', {
                 method: 'POST',
             });
             const data = await res.json();
-            if(!res.ok) {
+            if (!res.ok) {
                 console.log(data.message);
-            }else{
+            } else {
                 dispatch(signoutSuccess());
             }
         } catch (error) {
             console.lof(error.message);
         }
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
     return (
         <Navbar className='border-b-2'>
             <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
                 <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Yuelin's</span>
                 Blog
             </Link>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <TextInput
                     text='text'
                     placeholder='Search..'
                     rightIcon={AiOutlineSearch}
                     className='hidden lg:inline'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
             </form>
             <Button className='w-12 h-12 lg:hidden' color='gray'>
                 <AiOutlineSearch />
             </Button>
             <div className='flex gap-2 md:order-2'>
-                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toggleTheme())}>
-                    {theme === 'light' ? <FaSun /> : <FaMoon/>}
+                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={() => dispatch(toggleTheme())}>
+                    {theme === 'light' ? <FaSun /> : <FaMoon />}
                 </Button>
                 {currentUser ? (
-                    <Dropdown 
-                        arrowIcon ={false}
+                    <Dropdown
+                        arrowIcon={false}
                         inline
-                        label = {
-                            <Avatar alt = 'user' img = {currentUser.profilePicture} rounded />
+                        label={
+                            <Avatar alt='user' img={currentUser.profilePicture} rounded />
                         }
                     >
-                    <Dropdown.Header>
-                        <span className="block text-sm">@{currentUser.username}</span>
-                        <span className="block text-sm font-medium truncate">{currentUser.email}</span>
-                    </Dropdown.Header>
-                    <Link to={'/dashboard?tab=profile'}>
-                        <Dropdown.Item>Profile</Dropdown.Item>
-                        <Dropdown.Divider/>
-                        <Dropdown.Item onClick={handleSignout}>Sign Out</Dropdown.Item>
-                    </Link>
+                        <Dropdown.Header>
+                            <span className="block text-sm">@{currentUser.username}</span>
+                            <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+                        </Dropdown.Header>
+                        <Link to={'/dashboard?tab=profile'}>
+                            <Dropdown.Item>Profile</Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.Item onClick={handleSignout}>Sign Out</Dropdown.Item>
+                        </Link>
                     </Dropdown>
                 ) : (
                     <Link to='/sign-in'>
