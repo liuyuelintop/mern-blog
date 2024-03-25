ea6d02542836838a4ffb76d67e2b7198b8c5d429
liuyuelintop <liuyuelintop@gmail.com>
2024-03-25 17:25:58 +1100

    add like functionality to the comment component

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,8 +1,13 @@
 import moment from 'moment';
 import { useEffect, useState } from 'react';
+import { FaThumbsUp } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import { Button, Textarea } from 'flowbite-react';
 
-export default function Comment({ comment }) {
+
+export default function Comment({ comment, onLike }) {
   const [user, setUser] = useState({});
+  const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -18,19 +23,37 @@ export default function Comment({ comment }) {
     getUser();
   }, [comment]);
 
-  
-return (
+
+  return (
     <div className='flex p-4 dark:border-gry-600 text-sm'>
-        <div className='flex-shrink-0 mr-3'>
-            <img className='w-10 h-10 rounded-full bg-gray-200' src={user.profilePicture} alt={user.username} />
+      <div className='flex-shrink-0 mr-3'>
+        <img className='w-10 h-10 rounded-full bg-gray-200' src={user.profilePicture} alt={user.username} />
+      </div>
+      <div className='flex-1'>
+        <div className='flex items-center mb-1'>
+          <span className='font-bold mr-1 text-xs truncate'>{user ? `@${user.username}` : 'anonymous user'}</span>
+          <span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
         </div>
-        <div className='flex-1'>
-            <div className='flex items-center mb-1'>
-                <span className='font-bold mr-1 text-xs truncate'>{user? `@${user.username}`:'anonymous user'}</span>
-                <span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
-            </div>
-            <p className='mb-2 text-gray-500'>{comment.content}</p>
+        <p className='mb-2 text-gray-500'>{comment.content}</p>
+        <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
+          <button
+            type='button'
+            onClick={() => onLike(comment._id)}
+            className={`text-gray-400 hover:text-blue-500 ${currentUser &&
+              comment.likes.includes(currentUser._id) &&
+              '!text-blue-500'
+              }`}
+          >
+            <FaThumbsUp className='text-sm' />
+          </button>
+          <p className='text-gray-400'>
+            {comment.numberOfLikes > 0 &&
+              comment.numberOfLikes +
+              ' ' +
+              (comment.numberOfLikes === 1 ? 'like' : 'likes')}
+          </p>
         </div>
+      </div>
     </div>
-)
+  )
 }
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
@@ -9,6 +8,7 @@ export default function CommentSection({ postId }) {
   const [comment, setComment] = useState('');
   const [comments, setComments] = useState([]);
   const [commentError, setCommentError] = useState(null);
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (comment.length > 200) {
@@ -52,6 +52,34 @@ export default function CommentSection({ postId }) {
     getComments();
   }, [postId]);
   
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate('/sign-in');
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: 'PUT',
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ? (
@@ -117,6 +145,7 @@ export default function CommentSection({ postId }) {
             <Comment
               key={comment._id}
               comment={comment}
+              onLike={handleLike}
             />
           ))}
         </>
